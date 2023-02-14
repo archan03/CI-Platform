3042623dec8dfd8aa39182030eaa56cc3db17e45
archan03 <bhalaniarchan@gmail.com>
2023-02-14 09:54:21 +0530

    First commit

---
+++ CI Project/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CI_Project.Models;
+
+namespace CI_Project.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult login()
+    {
+        return View();
+    }
+
+    public IActionResult Forgot_password()
+    {
+        return View();
+    }
+
+    public IActionResult Registration()
+    {
+        return View();
+    }
+
+    public IActionResult reset()
+    {
+        return View();
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
